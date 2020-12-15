ffdce98f0f5166b47df2ceb56095b46b87d7aaa1
kkw0818 <kkw0818@gmail.com>
2020-12-15 11:23:14 +0900

    todayprofile에 획득 골드 추가

--- src/main/java/egovframework/example/sample/web/model/ProfileModel.java
+++ src/main/java/egovframework/example/sample/web/model/ProfileModel.java
@@ -7,6 +7,7 @@ public class ProfileModel {
     public long lose = 0;
     public long highgaingold = 0;
     public long gaingold = 0;
+    public long gainbalance = 0;
     public long putallin = 0;
     public long goldrefillcount = 1;
     public long chiprefillcount = 3;
