dc5c29b089dfa17173c6e481f11b02054c7add53
kkw0818 <kkw0818@gmail.com>
2020-12-18 10:35:15 +0900

    월 초기화 하게 변경

--- src/main/java/egovframework/example/sample/web/Attendance.java
+++ src/main/java/egovframework/example/sample/web/Attendance.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,10 +20,11 @@ public class Attendance {
     public int Midx;
     public int Count;
     public long NextTick;
+    public String Get;
 
     public static Attendance MakeAttendance(int midx)
     {
-        Attendance attendance = new Attendance(midx, 0, 0);
+        Attendance attendance = new Attendance(midx, 0, "", 0);
 
         EgovMap in = new EgovMap();
         in.put("midx", midx);
@@ -31,6 +33,7 @@ public class Attendance {
         if( ed != null )
         {
             attendance.Count = (int)ed.get("count");
+            attendance.Get = ed.get("get").toString();
             attendance.NextTick = (long)ed.get("nexttick");
         }
         else
@@ -44,23 +47,40 @@ public class Attendance {
         return attendance;
     }
 
-    public Attendance(int idx, int count, long tick)
+    public Attendance(int idx, int count, String get, long tick)
     {
         this.Midx = idx;
         this.Count = count;
         this.NextTick = tick;
+        this.Get = get;
     }
 
     public int CheckAttendance()
     {
+        if( this.Get.isEmpty() == true )
+        {
+            this.NextTick = 0;
+            this.Count = 0;
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        String yearmonth = String.format("%d-%d", now.getYear(), now.getMonthValue());
+
+        if( this.Get.equals(yearmonth) == false)
+        {
+            this.NextTick = 0;
+            this.Count = 0;
+        }
+
         if( this.NextTick > System.currentTimeMillis() )
         {            
             return this.Count;
         }
 
-        int count = this.Count;
+        int count = this.Count;        
 
-        this.NextTick = System.currentTimeMillis() - (System.currentTimeMillis() % 86400000) + 86400000;          
+        this.NextTick = System.currentTimeMillis() - (System.currentTimeMillis() % 86400000) + 86400000; 
+        this.Get = yearmonth;   
         
         ClassPathResource resource = new ClassPathResource("json/attendance.json");
         ObjectMapper mapper = new ObjectMapper();
@@ -103,14 +123,15 @@ public class Attendance {
 
             EgovMap searchin = new EgovMap();
             searchin.put("count", (this.Count + 1) % itemlist.size());
+            searchin.put("get", this.Get);
             searchin.put("nexttick", this.NextTick);
             searchin.put("midx", this.Midx);
 
             SocketHandler.sk.sampleDAO.update("UpdateAttendance", searchin);
-
-            this.Count = (this.Count + 1) % itemlist.size();            
-
-            count = this.Count;
+            if( this.Count < itemlist.size() )
+            {
+                count = (this.Count + 1);            
+            }            
 
         }
         catch (IOException e) {
